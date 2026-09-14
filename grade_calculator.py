1c297e6c32ae984f4a2759568ddf70e4dcad14a3
theofficialkamara <theofficialkamara@gmail.com>
2026-09-14 04:29:32 -0500

    Fix variable collision between loop and grades list

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,3 +1,9 @@
+student_grades = {
+    "Maya": [90, 85, 78],
+    "Sam": [70, 65, 80],
+    "Alex": [88, 92, 79],
+}
+
 grades = [90, 85, 78]
 print(grades[0])
 print(grades[2])    
@@ -10,3 +16,8 @@ print(round(average, 2))
 
 for grade in grades:
     print(grade) 
+
+for student_name, student_scores in student_grades.items():
+    print(student_name, student_scores)  
+
+print(grades)
