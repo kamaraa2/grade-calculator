cf27326a8e820112493d6db467794eab908d1c1d
theofficialkamara <theofficialkamara@gmail.com>
2026-09-15 21:52:53 -0500

    Assign letter grades using conditional statements

--- grade_calculator.py
+++ grade_calculator.py
@@ -2,16 +2,33 @@
 # CST 205 - Module 1 Assignment
 # Author: Abraham Kamara
 
-# Student data: each student's name maps to a list of their grades.
+
 student_grades = {
     "Maya": [90, 85, 78],
     "Sam": [70, 65, 80],
     "Alex": [88, 92, 79],
 }
 
-#Blank Dictionary
 student_averages = {}
 for name, scores in student_grades.items():
     student_averages[name] = sum(scores) / len(scores)
 
 print(student_averages) 
+
+student_letter_grades = {}
+
+for name, average in student_averages.items():
+    if average >= 90:
+        letter = "A"
+    elif average >= 80:
+        letter = "B"
+    elif average >= 70:
+        letter = "C"
+    elif average >= 60:
+        letter = "D"
+    else:
+        letter = "F"
+    
+    student_letter_grades[name] = letter
+
+print(student_letter_grades)
